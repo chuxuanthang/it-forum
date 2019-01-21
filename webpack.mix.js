a4922548364994b5598e26c75aa01d26af9673b5
Chu Xuan Thang <chu.xuan.thang@framgia.com>
2019-01-21 17:07:19 +0700

    deploy

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,7 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
 
-mix.extract(['jquery', 'bootstrap']);
+// mix.extract(['jquery', 'bootstrap']);
 
 mix.styles(
     [
